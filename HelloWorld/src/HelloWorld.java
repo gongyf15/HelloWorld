42675ad5bd178c8e5842fc671625b68b6feeafc7
Transino <Transino@Transino-PC>
2016-07-07 16:38:49 +0800

    add the ability to count number!

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -8,6 +8,9 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("I'm a new born boy!");
+		CountNum cn = new CountNum();
+		cn.Count(10);
+		System.out.println("I can already count num to 10!");
 	}
 
 }
